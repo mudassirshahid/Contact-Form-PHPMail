1e4dc33c0cb4049a94dd8a3fdef90be096de954f
Mudassir Shahid <mudassirshahid84@gmail.com>
2022-11-22 17:43:12 +0500

    email change

--- contact/sendemail.php
+++ contact/sendemail.php
@@ -18,13 +18,13 @@ if(isset($_POST['submit'])){
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'shahbaz.brandian@gmail.com'; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'qejmalwpjqbceahp'; // Gmail address Password
+    $mail->Username = 'mudassir.brandian@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'gtnyxbcevrwpszjv'; // Gmail address Password GENERATE PASSWORD ON YOUR GMAIL THAT YOU GIVEN HERE WITH THE HELP OF 2 STEP VERIFICATION
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = '587';
 
-    $mail->setFrom('shahbaz.brandian@gmail.com'); // Gmail address which you used as SMTP server
-    $mail->addAddress('shahbaz.brandian@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+    $mail->setFrom('mudassir.brandian@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('mudassir.brandian@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
     $mail->isHTML(true);
     $mail->Subject = 'Message Received (Contact Page)';
